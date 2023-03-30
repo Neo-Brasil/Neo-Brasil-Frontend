2794d23c3babba8fb16b656c7b7c91f42b334c12
Desduh <eduardofalandess@gmail.com>
2023-03-30 10:15:02 -0300

    integracao para delecao de cliente

--- src/components/ModalDelete/index.js
+++ src/components/ModalDelete/index.js
@@ -1,7 +1,19 @@
 import './ModalDelete.css';
 import { FiArrowLeft } from 'react-icons/fi';
+import Axios from "axios";
 
 export default function ModalDelete({close}){
+    const id_ = localStorage.getItem("id") ; 
+    const id = parseInt(id_);
+
+    console.log(id);
+    function handleSubmit() {
+        Axios.delete(`http://localhost:9080/excluir/cliente/${id}`).then((res) => {
+            console.log(res)
+        })
+        localStorage.clear();
+    }
+
     return(
         <div className="modal">
             <div className="container">
@@ -13,7 +25,7 @@ export default function ModalDelete({close}){
                     <p>Deseja deletar <b>Amanda Caires Perreira?</b></p>
                     <p className='aviso'>Esta ação não poderá ser desfeita!</p>
 
-                    <div className='button-color'>
+                    <div className='button-color' onClick={() => handleSubmit()}>
                         <button className='button-red'>DELETAR</button>
                     </div>
 
--- src/pages/CrudCliente/index.js
+++ src/pages/CrudCliente/index.js
@@ -1,24 +1,47 @@
 import './CrudCliente.css';
 import Header from "../../components/Header";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ModalDelete from '../../components/ModalDelete';
-
+import Axios from "axios";
 import { Link } from 'react-router-dom';
 import { MdContactPage, MdDeleteForever } from "react-icons/md";
 
 export default function CrudCliente() {
     const [showPostModal, setShowPostModal] = useState(false);
     const [detail, setDetail] = useState();
+    const [list, setList] = useState([]);
 
-    function togglePostModal() {
+    function togglePostModal(id) {
+        localStorage.clear();
+        localStorage.setItem("id", id);
         setShowPostModal(!showPostModal);
         setDetail();
     }
 
+    useEffect(() => {
+        Axios.get(`http://127.0.0.1:9080/listagem/clientes`).then((resp) => {
+          var dados = resp.data
+          var novoDados = []
+          for(var k in dados){
+              var novoDado = []
+              novoDado.push(dados[k].id)
+              novoDado.push(dados[k].nome);
+              novoDados.push(novoDado)
+          }
+          setList(novoDados);
+        });
+      }, [])
+
     return (
         <div>
             <Header />
+            {list.lenght === 0 ? (
+                <div className='none'>
+                <p>Nenhum dado encontrado...</p>
+                <p>Não há relatório diário!</p>
+                </div>
+            ) : (
             <div className="content">
 
                 <h1>Clientes cadastrados</h1>
@@ -33,26 +56,31 @@ export default function CrudCliente() {
                             </tr>
                         </thead>
 
-                        <tbody>
-                            <tr>
-                                <td data-label="Nome">Teste de nome</td>
+                        {typeof list !== 'undefined' && list.map((value) => {
+                            return !value.envio ?
+                            <tbody>
+                                <tr>
+                                    <td data-label="Nome">{value[1]}</td>
 
-                                <td data-label="Visualizar">
-                                    <Link to={'/clientes_cadastrados/visualizar_editar'}>
-                                    <MdContactPage color='#2F44FF' size={25} /></Link>
-                                </td>
+                                    <td data-label="Visualizar">
+                                        <Link to={'/clientes_cadastrados/visualizar_editar'}>
+                                        <MdContactPage color='#2F44FF' size={25} /></Link>
+                                    </td>
 
-                                <td data-label="Deletar">
-                                    <Link className="action" onClick={() => togglePostModal()}>
-                                    <MdDeleteForever color="#F42020" size={25} /></Link>
-                                </td>
+                                    <td data-label="Deletar">
+                                        <Link className="action" onClick={() => togglePostModal(value[0])}>
+                                        <MdDeleteForever color="#F42020" size={25} /></Link>
+                                    </td>
 
-                            </tr>
+                                </tr>
 
-                        </tbody>
+                            </tbody>
+                            :null
+                        })}
                     </table>
                 </div>
             </div>
+            )}
             {showPostModal && (
                 <ModalDelete conteudo={detail} close={togglePostModal} />
             )}
