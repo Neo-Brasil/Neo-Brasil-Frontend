d7c6da92f274d840edafc90f5c00739a356f32f2
Desduh <eduardofalandess@gmail.com>
2023-04-23 17:15:52 -0300

    correcao do erro na renderizacao das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,72 +21,67 @@ import SemAcesso from '../pages/SemAcesso/index.tsx';
 
 export default function RoutesApp(){
     const acesso = localStorage.getItem("acesso")
-    if(acesso == null || acesso == "NOVO" || acesso == 0){
+    if(acesso == "ADM"){
+        return(
+            <Routes> 
+                <Route path="/" element={ <Login/> } />                
+                <Route path="/aprovar_contas" element={ <AprovarConta/> } />
+                <Route path="/usuarios_cadastrados" element={ <CrudUsuario/> } />
+                
+                <Route path="/cadastro" element={ <CadastrarCliente/> } />
+                <Route path="/clientes_cadastrados" element={ <CrudCliente/> } />
+                <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <VerEditarCliente/> } />
+    
+                <Route path="/registrar_pagamento" element={ <RegistrarPago/> } />
+                <Route path="/relatorio" element={ <Relatorio/> } />
+                <Route path="/criar_conta" element={ <CriarConta/> } /> 
+
+                <Route path="*" element={ <Erro/> } />   
+            </Routes>
+        )
+    }else if(acesso == "COMERCIAL"){
+        return(
+            <Routes>     
+                <Route path="/" element={ <Login/> } />                
+                <Route path="/cadastro" element={ <CadastrarCliente/> } />
+                <Route path="/clientes_cadastrados" element={ <CrudCliente/> } />
+                <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <VerEditarCliente/> } />
+
+                <Route path="/relatorio" element={ <Relatorio/> } />    
+
+                <Route path="*" element={ <Erro/> } />
+
+                <Route path="/aprovar_contas" element={ <SemAcesso/> } />
+                <Route path="/usuarios_cadastrados" element={ <SemAcesso/> } />
+                <Route path="/registrar_pagamento" element={ <SemAcesso/> } />    
+                <Route path="/criar_conta" element={ <CriarConta/> } />       
+            </Routes>
+        )     
+    }else if(acesso == "FINANCEIRO"){
+        return(
+            <Routes>  
+                <Route path="/" element={ <Login/> } />       
+                <Route path="/registrar_pagamento" element={ <RegistrarPago/> } />
+                <Route path="/relatorio" element={ <Relatorio/> } />
+    
+                <Route path="*" element={ <Erro/> } />
+
+                <Route path="/cadastro" element={ <SemAcesso/> } />
+                <Route path="/clientes_cadastrados" element={ <SemAcesso/> } />
+                <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <SemAcesso/> } />
+                <Route path="/aprovar_contas" element={ <SemAcesso/> } />
+                <Route path="/usuarios_cadastrados" element={ <SemAcesso/> } />
+                <Route path="/criar_conta" element={ <CriarConta/> } /> 
+            </Routes>
+        )
+    }else{
         return(
             <Routes>
                 <Route path="/" element={ <Login/> } /> 
                 <Route path="/criar_conta" element={ <CriarConta/> } /> 
                 <Route path="/termosDeUso" element={ <Termos/> } /> 
+                <Route path="*" element={ <Erro/> } />
             </Routes>
         )
-    }else{
-        if(acesso == "ADM"){
-            return(
-                <Routes> 
-                    <Route path="/" element={ <Login/> } />                
-                    <Route path="/aprovar_contas" element={ <AprovarConta/> } />
-                    <Route path="/usuarios_cadastrados" element={ <CrudUsuario/> } />
-                    
-                    <Route path="/cadastro" element={ <CadastrarCliente/> } />
-                    <Route path="/clientes_cadastrados" element={ <CrudCliente/> } />
-                    <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <VerEditarCliente/> } />
-        
-                    <Route path="/registrar_pagamento" element={ <RegistrarPago/> } />
-                    <Route path="/relatorio" element={ <Relatorio/> } />
-                    <Route path="/criar_conta" element={ <CriarConta/> } /> 
-
-                    <Route path="*" element={ <Erro/> } />   
-                </Routes>
-            )
-        }else if(acesso == "COMERCIAL"){
-            return(
-                <Routes>     
-                    <Route path="/" element={ <Login/> } />                
-                    <Route path="/cadastro" element={ <CadastrarCliente/> } />
-                    <Route path="/clientes_cadastrados" element={ <CrudCliente/> } />
-                    <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <VerEditarCliente/> } />
-    
-                    <Route path="/relatorio" element={ <Relatorio/> } />    
-    
-                    <Route path="*" element={ <Erro/> } />
-    
-                    <Route path="/aprovar_contas" element={ <SemAcesso/> } />
-                    <Route path="/usuarios_cadastrados" element={ <SemAcesso/> } />
-                    <Route path="/registrar_pagamento" element={ <SemAcesso/> } />    
-                    <Route path="/criar_conta" element={ <CriarConta/> } />       
-                </Routes>
-            )     
-        }else if(acesso == "FINANCEIRO"){
-            return(
-                <Routes>  
-                    <Route path="/" element={ <Login/> } />       
-                    <Route path="/registrar_pagamento" element={ <RegistrarPago/> } />
-                    <Route path="/relatorio" element={ <Relatorio/> } />
-        
-                    <Route path="*" element={ <Erro/> } />
-    
-                    <Route path="/cadastro" element={ <SemAcesso/> } />
-                    <Route path="/clientes_cadastrados" element={ <SemAcesso/> } />
-                    <Route path="/clientes_cadastrados/visualizar_editar/:id" element={ <SemAcesso/> } />
-                    <Route path="/aprovar_contas" element={ <SemAcesso/> } />
-                    <Route path="/usuarios_cadastrados" element={ <SemAcesso/> } />
-                    <Route path="/criar_conta" element={ <CriarConta/> } /> 
-                </Routes>
-            )
-        }else{
-            return(
-                <Route path="*" element={ <Erro/> } />
-            )
-        }
-    }
+    }   
 }
