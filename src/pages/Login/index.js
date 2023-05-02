16725349bd9509ea7d4a7c5694a51414e13e49bd
amandavo <amandavoliveira0@gmail.com>
2023-05-02 10:43:32 -0300

    responsivo do login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import './Login.css';
 import logo from '../../assets/logo-transparent.png';
+import lotus from '../../assets/lotus.png';
 import React, { useState } from "react";
 import Axios from "axios";
 import { Link } from 'react-router-dom';
@@ -82,7 +83,10 @@ export default function Login() {
         <div>
             <div className="login">
                 <div className='logo'>
+                <picture>
+                    <source media="(max-width: 600px)" srcset={lotus} />
                     <img src={logo} alt="logo de folhagem e escrito Neo-Brasil" />
+                </picture>
                 </div>
 
                 <form onSubmit={handleSubmit}>
