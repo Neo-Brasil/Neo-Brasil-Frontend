19cadde70dad37d1b16d6724b3f6abd0105a0f61
Desduh <eduardofalandess@gmail.com>
2023-05-25 17:47:51 -0300

    finalizacao da tabela de relatorios

--- src/pages/Relatorio/index.js
+++ src/pages/Relatorio/index.js
@@ -18,34 +18,45 @@ import VerificaToken from '../../script/verificaToken';
 export default function Relatorio() {
     const [showPostModal, setShowPostModal] = useState(false);
     const [detail, setDetail] = useState();
+    const [valorCreditado, setValorCreditado] = useState(0);
+    const [valorPago, setValorPago] = useState(0);
+    const [valorEmAbarto, setValorEmAbarto] = useState(0);
+    const [valorAtrasado, setValorAtrasado] = useState(0);
+    const [valorTotal, setValorTotal] = useState(0);
 
-    const [clientes, setClientes] = useState([]);
-    const [valorRecebido, setValorRecebido] = useState(0);
-    const [valorReceber, setValorReceber] = useState(0);
-    const [valorCreditar, setValorCreditar] = useState(0);
     const [intervalo, setIntervalo] = useState("Todas");
 
     const [dataInicio, setDataInicio] = useState('0000-00-00');
     const [dataFim, setDataFim] = useState('0000-00-00');
 
-    const initialData = [
-        { id: 1, name: 'João Doe', age: 25 },
-        { id: 2, name: 'Zacariah', age: 30 },
-        { id: 3, name: 'Bob Johnson', age: 35 },
-        { id: 4, name: 'Jane Doe', age: 25 },
-        { id: 5, name: 'Amanda Smith', age: 30 },
-        { id: 6, name: 'Vini Junior', age: 35 },
-        { id: 11, name: 'Carlos Silva', age: 25 },
-        { id: 21, name: 'Julio Carvalho', age: 30 },
-        { id: 31, name: 'Elen Watson', age: 35 },
-        { id: 41, name: 'Lucas Barreto', age: 25 },
-        { id: 51, name: 'Julia Simões', age: 30 },
-        { id: 61, name: 'Gerson Noronha', age: 35 }
-    ];
-
-    const [data, setData] = useState(initialData);
+    const [data, setData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
+    useEffect(() => {
+        listagemPrestacoes();
+        Axios.get(`http://localhost:9080/listagem/clientes`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+        }).catch(function (error) {
+            VerificaToken(error)
+        }).then((resp) => {
+            var prestacoes = [];
+            let clientes = resp.data;
+            for (let C in clientes) {
+                clientes[C].titulos[0].preco = clientes[C].titulos[0].preco.toFixed(2).toString().replace(".", ",");
+                for (let T in clientes[C].titulos){
+                    for (let P in clientes[C].titulos[T].prestacoes){
+                        let prestacao = clientes[C].titulos[T].prestacoes[P]
+                        let prestacaoNova = { id: prestacao.id, nome: clientes[C].nome, vencimento: prestacao.data_vencimento, pagamento: prestacao.data_pagamento, status: prestacao.situacao, preco: prestacao.preco }
+                        prestacoes.push(prestacaoNova)
+                    }
+                }
+            }
+            setData(prestacoes)
+        });
+    }, [])
 
     const itemsPerPage = 6;
     const totalPages = Math.ceil(data.length / itemsPerPage);
@@ -118,42 +129,33 @@ export default function Relatorio() {
                     VerificaToken(error)
                 }).then((resp) => {
                     var dado = resp.data
-                    let receber = dado.receber
-                    let recebido = dado.recebido
-                    let creditar = dado.creditar
+                    let creditado = dado.creditado
+                    let pago = dado.pago
+                    let emAbarto = dado.emAberto
+                    let atrasado = dado.atrasado
+                    let total = parseFloat(creditado) + parseFloat(pago) + parseFloat(emAbarto) + parseFloat(atrasado)
 
-                    receber = parseFloat(receber).toFixed(2);
-                    recebido = parseFloat(recebido).toFixed(2);
-                    creditar = parseFloat(creditar).toFixed(2);
+                    creditado = parseFloat(creditado).toFixed(2);
+                    pago = parseFloat(pago).toFixed(2);
+                    emAbarto = parseFloat(emAbarto).toFixed(2);
+                    atrasado = parseFloat(atrasado).toFixed(2);
+                    total = parseFloat(total).toFixed(2);
 
-                    receber = receber.toString().replace(".", ",");
-                    recebido = recebido.toString().replace(".", ",");
-                    creditar = creditar.toString().replace(".", ",")
+                    creditado = creditado.toString().replace(".", ",");
+                    pago = pago.toString().replace(".", ",");
+                    emAbarto = emAbarto.toString().replace(".", ",");
+                    atrasado = atrasado.toString().replace('.',',');
+                    total = total.toString().replace('.',',');
 
-                    setValorReceber(receber);
-                    setValorRecebido(recebido);
-                    setValorCreditar(creditar);
+                    setValorCreditado(creditado);
+                    setValorPago(pago);
+                    setValorEmAbarto(emAbarto);
+                    setValorAtrasado(atrasado);
+                    setValorTotal(total);
                 });
             }
         });
     }
-
-    useEffect(() => {
-        listagemPrestacoes();
-        Axios.get(`http://localhost:9080/listagem/clientes`, {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem("token")}`
-            }
-        }).catch(function (error) {
-            VerificaToken(error)
-        }).then((resp) => {
-            let cliente = resp.data;
-            for (let k in cliente) {
-                cliente[k].titulos[0].preco = cliente[k].titulos[0].preco.toFixed(2).toString().replace(".", ",");
-            }
-            setClientes(cliente)
-        });
-    }, [])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -173,7 +175,7 @@ export default function Relatorio() {
         <div>
         <Header />
 
-        {clientes.lenght === 0 ? (
+        {data.lenght === 0 ? (
 
             <div className='none'>
                 <p>Nenhum relatório encontrado...</p>
@@ -219,7 +221,6 @@ export default function Relatorio() {
 
                 <details>
                     <summary>Filtro de situação</summary>
-
                     <div className='filter'>
                         <select onChange={(e) => setIntervalo(e.target.value)} required>
                             <option value="Todas">Todas as situações</option>
@@ -228,15 +229,6 @@ export default function Relatorio() {
                             <option value="Crédito">Crédito</option>
                         </select>
                     </div>
-
-                    {/* <div className='filter'>
-                        <input type="radio" value="HTML"></input>
-                            <label for="html">HTML</label>
-                        <input type="radio" value="CSS"></input>
-                            <label for="css">CSS</label>
-                        <input type="radio" value="JavaScript"></input>
-                            <label for="javascript">JavaScript</label>
-                    </div> */}
                 </details>
             </div>
             </form>
@@ -257,8 +249,11 @@ export default function Relatorio() {
                         {currentItems.map((item) => (
                             <tr key={item.id}>
                                 <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.age}</td>
+                                <td>{item.nome}</td>
+                                <td>{item.vencimento}</td>
+                                <td>{item.pagamento}</td>
+                                <td>{item.status}</td>
+                                <td>{item.preco}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -283,65 +278,22 @@ export default function Relatorio() {
                         <FiChevronRight />
                     </button>
                 </div>
-            </div>
-
-                    {/* <div className='container-table' id='relatorio-table'>
-                        <table>
-                            <thead>
-                                <tr>
-                                    
-                                </tr>
-                            </thead>
-
-                            {typeof clientes !== 'undefined' && clientes.map((value) => {
-                                return !value.envio ?
-
-                                    <tbody>
-                                        <tr className='noMargin'>
-                                            <td data-label="Nome">{value.nome}</td>
-
-                                            <td data-label="Prestações">
-                                                <Link className="action" onClick={() => togglePostModal(value.titulos[0].id)}>
-                                                    <FiLayers color="#F79736" size={30} />
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                            : null
-                            })}
-                        </table>
-                    </div> */}
-
+                </div>
                     <div className='financialArea'>
                         <div className='values'>
-                            <i>Total em aberto: { }</i>
-                            <i>Total atrasado: { }</i>
+                            <i>Total em aberto: { valorEmAbarto }</i>
+                            <i>Total atrasado: { valorAtrasado }</i>
                         </div>
 
                         <div className='values'>
-                            <i>Total creditado: { }</i>
-                            <i>Total pago: { }</i>
+                            <i>Total creditado: { valorCreditado }</i>
+                            <i>Total pago: { valorPago }</i>
                         </div>
 
                         <div className='values'>
-                            <i>Total: { }</i>
+                            <i>Total: { valorTotal }</i>
                         </div>
                     </div>
-
-                    {/* <tbody>
-                                    <tr id='nostyleinput' className='noMargin'>
-                                        <td data-label="Valor à receber">
-                                            <MaskedInput mask={currencyMask} className="nostyleinput"
-                                                type="text" placeholder="R$" value={valorReceber} disabled /></td>
-                                        <td data-label="Valor recebido">
-                                            <MaskedInput mask={currencyMask} className="nostyleinput"
-                                                type="text" placeholder="R$" value={valorRecebido} disabled /></td>
-                                        <td data-label="Valor à creditar">
-                                            <MaskedInput mask={currencyMask} className="nostyleinput"
-                                                type="text" placeholder="R$" value={valorCreditar} disabled /></td>
-                                    </tr>
-                                </tbody> */}
-
                 </div>
             )}
             {showPostModal && (
